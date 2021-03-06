97fc58b01eb0cd669b41b90caa00ba074c0c2450
Eugene <eugenevdm@gmail.com>
2021-03-06 21:54:08 +0200

    more

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+        
     }
 }
