3570ee6b981d2e3226492ef3c5903d3f71e8881a
Eugene <eugenevdm@gmail.com>
2021-03-07 09:53:25 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('/chat-bot', '\App\Http\Controllers\WhatsAppController@listenToReplies');
 Route::post('/chat-bot', '\App\Http\Controllers\WhatsAppController@handle');
 
